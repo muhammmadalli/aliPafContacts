533cd59be6aeddab94d9bd85fa0b105d78ba7701
Muhammad Ali <105352723+muhammmadalli@users.noreply.github.com>
2026-04-27 02:11:01 +0500

    Bump version to 1.0.3.5 for XMPP display in contacts instead of JABBER

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.3"
+        versionName = "1.0.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
