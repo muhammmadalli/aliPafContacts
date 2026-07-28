dd4eb189ecf2dec0d2dc5ce195cdafbcccd9b287
Muhammad Ali <105352723+muhammmadalli@users.noreply.github.com>
2026-07-29 00:02:12 +0500

    Bump app version to 1.0.10
    Update app/build.gradle.kts: bump versionCode to 100100 and versionName to 1.0.10. Also change versionNameSuffix from "blueIcon" to "rndmInterval30-45days". No other functional changes.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,11 +20,11 @@ android {
         applicationId = "ali.paf.contacts"
         minSdk = 24
         targetSdk = 34
-        versionCode = 100093
-        versionName = "1.0.9"
+        versionCode = 100100
+        versionName = "1.0.10"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        versionNameSuffix = "blueIcon"
+        versionNameSuffix = "rndmInterval30-45days"
     }
 
     buildTypes {
